66727deb3b9b1af2dd1469e76f56c84c97879b97
arweber1 <arweber_93@hotmail.com>
2014-02-06 16:06:59 -0700

    did some stuff

--- src/ualberta/cmput301/camerademo/CameraDemoActivity.java
+++ src/ualberta/cmput301/camerademo/CameraDemoActivity.java
@@ -1,10 +1,15 @@
 package ualberta.cmput301.camerademo;
 
+import java.io.File;
+
 import ualberta.cmput301.camerodemo.R;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,6 +21,8 @@ public class CameraDemoActivity extends Activity {
 	private TextView textView;
 	private ImageButton imageButton;
 	private Uri imageFileUri;
+	String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,12 +47,31 @@ public class CameraDemoActivity extends Activity {
 	// Notice that startActivity() method will not return any result when the launched activity 
 	// finishes, while startActivityForResult() method will. To retrieve the returned result, you may 
 	// need implement onAcitityResult() method.
+	
+	
 	public void takeAPhoto() {
-		// To Do		
+		// To Do
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		File file = new File(path + "/Pictures/test.jpg");
+		
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+		startActivityForResult(intent, 0);
 	}
 	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// To Do
+		if (requestCode == 0){
+			if (resultCode == RESULT_OK){
+				Bitmap bm  = (Bitmap) data.getExtras().getParcelable("data");
+				imageButton.setImageBitmap(bm);
+				textView.setText("Photo OK");
+			} else if (resultCode == RESULT_CANCELED){
+				textView.setText("Photo Canceled");
+			}else{
+				textView.setText("Not sure what happened");
+			}
+				
+		}
 	}	
 	
 	@Override
